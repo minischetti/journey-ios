0ae47364d3cfe8c1133c4b9aa62a491c3ea48586
Dominic Minischetti <dominic_minischetti@anfcorp.com>
2023-12-25 13:28:54 -0800

    Update new item form

--- Journey/ContentView.swift
+++ Journey/ContentView.swift
@@ -11,7 +11,7 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
-    
+
     var body: some View {
         TabView {
                 Text("Home")
@@ -19,25 +19,92 @@ struct ContentView: View {
                     Image(systemName: "house")
                 }
                 List {
-                    Text("Lists")
+                    ForEach(items) { item in
+                        Text(item.title)
+                    }
+                    .onDelete { indexSet in
+                        indexSet.forEach { index in
+                            modelContext.delete(items[index])
+                        }
+                    }
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .listStyle(PlainListStyle())
+                    .navigationTitle("Items")
+#if iOS
+                    .toolbar {
+
+                            EditButton()
+
+                    }
+                    .environment(\.editMode, .constant(.active))
+                    .navigationBarTitleDisplayMode(.inline)
+                    .navigationBarItems(trailing: EditButton())
+#endif
                 }
                 .tabItem {
                     Image(systemName: "list.bullet")
                 }
+                FormSection()
 
-            
-                
+        }
+
+    }
+
+    struct FormSection: View {
+        @Environment(\.modelContext) private var modelContext
+        @Query private var items: [Item]
+        @State var title = ""
+        @State var content = ""
+        @State var dueDate = Date()
+        @State var repeatInterval = 0
+        @State var repeatUnit = RepeatUnit.day
+
+        func save() {
+            let item = Item(title: title, content: content, status: Status.pending, creationDate: Date(), dueDate: dueDate)
+            modelContext.insert(item)
+        }
+
+        var body: some View {
                 Form {
                     Section {
-                        TextField("Title", text: .constant(""))
-                        
+                        TextField("Title", text: $title)
+                        TextField("Content", text: $content)
+                        DatePicker("Due Date", selection: .constant(Date()))
+                    }
+                    Section {
+
+                        // Repeat every {interval} {unit} until {end} or {count} times
+                        Stepper(value: $repeatInterval) {
+                            Text("Repeat every \(repeatInterval) \(repeatUnit.rawValue)(s)")
+                        }
+                        Picker("Unit", selection: $repeatUnit) {
+                            Text("Day").tag(RepeatUnit.day)
+                            Text("Week").tag(RepeatUnit.week)
+                            Text("Month").tag(RepeatUnit.month)
+                            Text("Year").tag(RepeatUnit.year)
+                        }
+                        DatePicker("End", selection: .constant(Date()))
+                    }
+                    Section {
+                        Button("Save") {
+                            save()
+                        }
                     }
                 }
                 .tabItem {
                     Image(systemName: "plus")
                 }
         }
+    }
 
+
+
+    var newItemForm: some View {
+        Picker("Status", selection: .constant(0)) {
+            Text("Pending").tag(0)
+            Text("Working").tag(1)
+            Text("Done").tag(2)
+        }
     }
 }
 
